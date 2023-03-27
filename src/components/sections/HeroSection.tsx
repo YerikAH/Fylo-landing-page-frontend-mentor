b05480ff22e07d670df1cbb85b83b6cae24ba76f
Harvey Yerik <yerik05vh@gmail.com>
2023-03-27 14:13:50 -0500

    dev: update project

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -4,14 +4,19 @@ const HeroSection = () => {
   return (
     <section className='grid place-items-center w-full'>
       <img src={heroIllustration} alt='illustration intro' />
-      <h1 className='text-white text-2xl font-serif text-center font-bold'>
+      <h1 className='text-white text-2xl font-serif text-center font-bold mt-4'>
         All your files in one secure location, accessible anywhere
       </h1>
-      <p className='text-white text-sm font-sans text-center'>
+      <p className='text-white text-sm font-sans text-center mt-4 px-4'>
         Fylo stores all your most important files in one secure location. Access them wherever you
         need, share and collaborate with friends family, and co-workers.
       </p>
-      <button tabIndex={1}>Get Started</button>
+      <button
+        tabIndex={1}
+        className='mt-8 text-white text-sm py-4 px-20  text-center  font-bold  font-serif button-started'
+      >
+        Get Started
+      </button>
     </section>
   )
 }
