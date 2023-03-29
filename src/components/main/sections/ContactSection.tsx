d65c07076876f656d7520946213c5306020c1814
Harvey Yerik <yerik05vh@gmail.com>
2023-03-28 21:40:29 -0500

    dev: update project

---
+++ src/components/main/sections/ContactSection.tsx
@@ -0,0 +1,17 @@
+const ContactSection = () => {
+  return (
+    <article>
+      <h3>Get early access today</h3>
+      <p>
+        It only takes a minute to sign up and our free starter tier is extremely generous. If you
+        have any questions, our support team would be happy to help you
+      </p>
+      <div className='box-input'>
+        <input type='text' placeholder='email@example.com' />
+        <input type='submit' value='Get Started For Free' />
+      </div>
+    </article>
+  )
+}
+
+export default ContactSection
